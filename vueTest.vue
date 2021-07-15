a313eae3d460e94ed821a82432c7aa6881973dcc
Jacob West <jwest@omnitech-inc.com>
2021-07-15 09:25:13 -0500

    Basic Examples

---
+++ vueTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'vueTest',
+    data () {
+        return {
+            msg: 'Hello Vue!'
+        }
+    }
+}
+
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
